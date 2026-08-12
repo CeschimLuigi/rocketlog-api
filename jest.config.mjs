32a7a8ec9801f77d2d74a09d97f5eece74892717
TaakLuigi <luigi.ceschim@taak.com.br>
2026-08-12 13:29:02 -0300

    chore: update dependencies and configuration
    - Updated package.json to include new dependencies: @prisma/adapter-pg, @swc/core, @swc/jest, and dotenv.
    - Removed express-async-errors from dependencies.
    - Adjusted scripts for consistency and clarity.
    - Modified prisma/schema.prisma to remove the DATABASE_URL environment variable.
    - Removed express-async-errors import from src/app.ts.
    - Enhanced prisma.ts to utilize PrismaPg adapter with DATABASE_URL.
    - Added jest.config.mjs for Jest configuration with SWC support.
    - Created prisma.config.ts for Prisma configuration with environment variable support.

---
+++ jest.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('jest').Config} */
+export default {
+  bail: true,
+
+  clearMocks: true,
+
+  coverageProvider: "v8",
+
+  testEnvironment: "node",
+
+  testMatch: ["<rootDir>/src/**/*.test.ts"],
+
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+
+  transform: {
+    "^.+\\.ts$": [
+      "@swc/jest",
+      {
+        jsc: {
+          parser: { syntax: "typescript" },
+          target: "es2022",
+        },
+      },
+    ],
+  },
+}
